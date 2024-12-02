e70a6b78457d4a17919f66f0e5e06c85afafe527
Et <Et@et.com>
2024-12-03 00:15:53 +0800

    Add GitHub Pages deploy configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Budget from '../views/Budget.vue'
 import Bids from '../views/Bids.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/amz-tools-web/'  // 替换成你的仓库名
+    : '/'
+} 
